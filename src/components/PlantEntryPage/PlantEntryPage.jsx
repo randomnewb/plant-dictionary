a88f92595832b3374ecea88fae7d703284f96d8d
Keith Cullinan <kfcullinan@gmail.com>
2023-01-31 18:57:53 -0600

    started plantEntryPage form/values

---
+++ src/components/PlantEntryPage/PlantEntryPage.jsx
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import Card from "@mui/material/Card";
+import "../App/App.css";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+function PlantEntryPage() {
+    const [commonName, setCommonName] = useState(null);
+    const [botanicalName, setBotanicalName] = useState(null);
+    const [hardnessZone, setHardnessZone] = useState(null);
+    const [leafColors, setLeafColors] = useState(null);
+    const [flowerColors, setFlowerColors] = useState(null);
+    const [minimumHeight, setMinimumHeight] = useState(null);
+    const [maximumHeight, setMaximumHeight] = useState(null);
+    const [minimumSpread, setMinimumSpread] = useState(null);
+    const [maximumSpread, setMaximumSpread] = useState(null);
+    const [plantProperties, setPlantProperties] = useState(null);
+}
